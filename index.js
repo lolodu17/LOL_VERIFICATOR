436dab26dbef9c2f03a6681dcdee9b2dc30bfaf8
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-09 12:33:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const client = new Discord.Client()
 
+bot.on("ready", function () {
+  prefix = "!"
+.then(user => console.log(`j'ai choisis mon avatar`))
+})
+
 bot.login(process.env.TOKEN)
