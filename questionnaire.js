69f52b4d9437c4324287957495ce46366024833d
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-09 13:35:57 +0100

    Create questionnaire.js

---
+++ questionnaire.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const client = new Discord.Client()
+
+bot.on('message', message => {
+exports.run = () => {
+if(message.content.startsWith('!questionnaire'))
+{
+
+}else
+{
+
+}
+}}})
