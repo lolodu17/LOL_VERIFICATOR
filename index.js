14f27b6489e784e8edfe3b9f31181a91c4be0dd0
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-09 13:22:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ bot.on('guildMemberAdd', member => {
          let rol = member.guild.roles.find("name", "...")
           member.addRole(rol)
         channel.send("Bourjour, merci de bien répondre __correctement__ au questionnaire. \n Pour débuter le questionnaire faite !questionnaire")
-        var questionnaire = member.content.startsWith('!questionnaire');
-        if(questionnaire){
-          channel.send("Bourjour, merci de bien répondre __correctement__ au questionnaire. \n Pour débuter le questionnaire faite !questionnaire")
-        }else{
-          channel.send("tant pis !")
-}
-        
+                Jeux.run(); 
       }).catch(console.error)
     })
 
