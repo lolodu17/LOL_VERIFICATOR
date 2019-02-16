fea74c242f7eaab4812521f425f56206fc2f0b54
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-16 12:25:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const client = new Discord.Client()
-const Questionnaire = require('./questionnaire.js')
 
 bot.on("ready", function () {
   prefix = "!"
@@ -13,11 +12,21 @@ bot.on('guildMemberAdd', member => {
       member.createDM().then(channel => {
          let rol = member.guild.roles.find("name", "...")
           member.addRole(rol)
-        channel.send("Bourjour, merci de bien répondre __correctement__ au questionnaire. \n Pour débuter le questionnaire faite !questionnaire")
-                Questionnaire.run(); 
+        channel.send("Bonjour et bienvenue dans LoLTeamsFR, Vous voulez rejoindre la team ?\n\nBien !\n\n Alors, tous ce que tu a à faire, c'est d'allez dans le salon vérification et d'écrire !rejoindre !")
+        channel.startTyping();
       }).catch(console.error)
     })
-
+bot.on('message', message => {
+ if(message.content.startsWith('!rejoindre')){
+  if(message.channel.name !== "chatuni"){
+  message.member.sendMessage("Avant de rejoindre la team tu doit répondre à un questionnaire te consérnant (les information ne sera distribuée à d'autre membre.")
+  message.member.sendMessage("Fait la commande !ready pour remplir le questionnaire.\n\n tu as 15 seconde pour de répondre !")
+    const filter = message => message.content.startsWith('!ready');
+  }else{
+  
+  }
+  
+}})
 
 
 bot.login(process.env.TOKEN)
