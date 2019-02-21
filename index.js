4301ed9c89062f3740fa34feaac126f1285abe69
Octokling(1).js <34281237+lolodu17@users.noreply.github.com>
2019-02-21 12:52:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const client = new Discord.Client()
+  
 bot.on("ready", function () {
   prefix = "!"
   bot.user.setUsername('LoL VERIFICATOR').catch(console.error)
 .then(user => console.log(`j'ai choisis mon avatar`))
 })
 
+bot.on('message', message => {
+  var name = ("")
+    if(message.content.startsWith("!Prêt")){
+        
+        if(message.author.bot) return;
+        let role = message.guild.roles.find("name", "Membre");
+        if(message.member.roles.has(role.id)) return 
+        let test = message.content.split(" ");
+        let testinfo = test.slice(1);
+        let pseudo = testinfo.join(" ").slice(0)
+        message.member.send(`Quelle est votre nom dans League of Leagend ?`)
+      
+        name = (`${pseudo}`)
+      
+ var joueur_embed = new Discord.RichEmbed()
+        .setColor("#00CB0E")
+        .setTitle(`Nouveau joueur | ${message.author.username}`)
+        .addField("Prénom :",`${name}`)
+        .setFooter("Bienvenue à lui !")
+        message.member.send(joueur_embed)
+})
 bot.on('guildMemberAdd', member => {
       member.createDM(5000).then(channel => {
          let rol = member.guild.roles.find("name", "...")
@@ -22,41 +44,26 @@ const octokling = "490780128234831906";
  if(message.content.startsWith('!rejoindre') && message.channel.name == "vérification"){
    message.delete(message.author)
   message.member.sendMessage("Avant de rejoindre la team tu doit répondre à un questionnaire te consérnant (les information ne sera distribuée à d'autre membre.).\n\n")
-  message.member.sendMessage("\n\n\nFaite la commande __!ready__ __dans vérification__ pour remplir le questionnaire.\n\nTu as __1 minute__ pour de répondre !\n\n")
-  const filter = message => message.content.startsWith('!ready')
+  message.member.sendMessage("\n\n\nFaite la commande __!prêt__ __dans vérification__ pour remplir le questionnaire.\n\nTu as __1 minute__ pour de répondre !\n\n")
+  const filter = message => message.content.startsWith('!prêt')
 message.channel.awaitMessages(filter, {max: 1, time: 60000, errors: ['time'] })
      
    .then(collected => { 
+
   message.delete(message.author)
 let verification = message.guild.channels.find(`name`, "vérification");
 if(!verification) return message.member.send("Je n'ai trouvé pas le salon 'vérification'");
   let rol = message.guild.roles.find("name", "...")
 verification.send(`@${rol.name}, ${message.author} va remplir le questionnaire !`)
-  message.member.send("Quelle est pseudo dans le jeu ?\n\nQuelle âge avaient vous ?\n\nPourquoi avez vous rejoint le serveur ?\n\nQuelle est votre personnage favoris (celui que vous utiliser tout le temps) dans le jeu ?\n\nQuelle niveau êtes vous dans le jeu ?\n\nVous seriez interresser si on organise des événement sur ce serveur consénant le jeu ?\n\nVous avez un Ordinateur faible ; puissant ; gameur ?\n\nVous devez envoyer le questionnaire dans vérification et faire \n\nVous avez 30 minute pour répondre !")})
+  message.member.send("Quelle âge avaient vous ?\n\nPourquoi avez vous rejoint le serveur ?\n\nQuelle est votre personnage favoris (celui que vous utiliser tout le temps) dans le jeu ?\n\nQuelle niveau êtes vous dans le jeu ?\n\nVous seriez interresser si on organise des événement sur ce serveur consénant le jeu ?\n\nVous avez un Ordinateur faible ; puissant ; gameur ?\n\nVous devez envoyer le questionnaire dans vérification et faire \n\nVous avez 30 minute pour répondre !")
+
+})
+   
+   
 .catch(collected => {
   message.member.sendMessage("\n\nVous avez pas écrit à temps !\n\nVeuillez recommancer !")
 })
-    const filtrer = message.content.startsWith(`!questionnaire`)
-message.channel.awaitMessages(filtrer, {max: 1, time: 1800000, errors: ['time'] })  
-  .then(collected => {
-message.member.send("Merci d'avoir pris temps de nous répondre, nos modérateur vont certifier votre questionnaire !")
-message.guild.member(octokling).send(`${espace}`)
-})
-    .catch(collected => {
-message.member.send("Vous n'avait répondue au questionnaire à temps vous devez tout recommencer !")
-})
-
-
   
-}})
-bot.on('message', message => {
-  if(message.content.startsWith('!test')){
-  const filter = message => message.content.startsWith('!vote');
-// Errors: ['time'] treats ending because of the time limit as an error
-message.channel.awaitMessages(filter, { max: 4, time: 60000, errors: ['time'] })
-  message.channel.awaitMessages(filter, options = {})
-  .then(collected => console.log(collected.size))
-  .catch(collected => console.log(`After a minute, only ${collected.size} out of 4 voted.`));
 }})
 
 bot.login(process.env.TOKEN)
